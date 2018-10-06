031108658d5769d49d55692b4476e58e7499e1f8
Koji AGAWA <atty303@gmail.com>
2018-10-07 07:22:48 +0900

    vue add quasar

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import './styles/quasar.styl'
+import lang from 'quasar-framework/i18n/ja'
+import 'quasar-extras/material-icons'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}, i18n: lang
+ })
+
 Vue.config.productionTip = false;
 
 new Vue({
